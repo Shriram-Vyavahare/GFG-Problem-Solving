bd67a0d352f609c99b1d0892f75243b399745b31
Shriram-Vyavahare <shriramprabhakarvyavahare07@gmail.com>
2025-08-05 11:22:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Pattern 1/pattern-1.java
+++ Difficulty: Basic/Pattern 1/pattern-1.java
@@ -4,9 +4,9 @@ class Solution {
         // code here
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-             System.out.print("* ");   
+                System.out.print("* ");
             }
             System.out.println();
         }
     }
-}
+} 
